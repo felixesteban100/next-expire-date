007881f3ab62d25716dd3346d878508ac0c20783
felixesteban100 <felixesteban100@gmail.com>
2023-12-12 20:25:45 -0500

    redirect when logged in

--- auth.config.ts
+++ auth.config.ts
@@ -20,6 +20,8 @@ export const authConfig = {
         return false; // Redirect unauthenticated users to login page
       } 
       else if (isLoggedIn) {
+        const isOnLogin = nextUrl.pathname.startsWith("/login");
+        if(isOnLogin) return Response.redirect(new URL("/products/list", nextUrl));
         // return Response.redirect(new URL("/products/list", nextUrl));
         return true
         // return Response.redirect(new URL("/", nextUrl));
